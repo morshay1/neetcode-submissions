5af03e403eb9afb7195466f41fc26abbffff810a
Mor Shay <morshay99@gmail.com>
2026-09-23 18:46:36 +0300

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        prev = self.subsets(nums[1::])
+        return prev + [[nums[0]] + x for x in prev] 
+        # dont_take = self.subsets(nums[1::])
+        # take = []
+        # for sub in dont_take:
+        #     new_subset = [nums[0]] + sub
+        #     take.append(new_subset)
+        
+        # return take + dont_take
+
+
+        
